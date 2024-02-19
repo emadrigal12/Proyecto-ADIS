0561467024fd9150cba542550b58b0b531246d43
Emiliano Madrigal <138079102+emadrigal12@users.noreply.github.com>
2024-02-19 16:09:50 -0600

    update gitignore

---
+++ ProyectoADIS/Controllers/Portfolio.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoADIS.Controllers
+{
+    public class Portfolio : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
