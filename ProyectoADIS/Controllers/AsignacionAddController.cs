21de897cb1c67943d382c24d772eaf2eb2bc10e7
HyperP2502 <59908026+Charlie2502@users.noreply.github.com>
2024-02-19 19:18:48 -0600

    Create AsignacionAddController.cs

---
+++ ProyectoADIS/Controllers/AsignacionAddController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoADIS.Controllers
+{
+    public class AsignacionAddController : Controller
+    {
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+    }
+}
