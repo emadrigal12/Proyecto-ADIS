c1dcb529e73dc28b4a8455e098c1655d3668d3f9
Emiliano Madrigal <138079102+emadrigal12@users.noreply.github.com>
2024-02-22 18:57:26 -0600

    update gitignore

--- ProyectoADIS/obj/Debug/net8.0/ProyectoADIS.AssemblyInfo.cs
+++ ProyectoADIS/obj/Debug/net8.0/ProyectoADIS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoADIS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f295c763f934896038942eb3c8ce360cae222688")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64184253879f3370d95705bb09b5e75ea61dee7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoADIS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoADIS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
