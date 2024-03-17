04d672faa477cef118685bd5115981d0c21099ab
Emiliano Madrigal <138079102+emadrigal12@users.noreply.github.com>
2024-03-17 14:50:50 -0600

    add tarea controller and view

---
+++ ProyectoADIS/Controllers/TareaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoADIS.Controllers
+{
+    public class TareaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
